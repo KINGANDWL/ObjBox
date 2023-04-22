7b3b39e9b9c7719712e7f9e486b2a766412cfd4c
KING <1846998803@qq.com>
2023-04-22 19:01:56 +0800

    修复Helper判断组件错误

--- ObjBoxHelper/ObjBoxHelper.ts
+++ ObjBoxHelper/ObjBoxHelper.ts
@@ -102,8 +102,10 @@ export class ObjBoxHelper {
      */
     public static doesComponentHaveClassAnnotation(annotationName: string, component: ComponentInterface): boolean {
         if (annotationName != null && ObjBoxHelper.isObjectTypeofComponent(component)) {
-            if (component._annotations_.scannedTemplate.originalType == ComponentOriginalType.Component) {
-                return component._annotations_.clazz.getAnnotation(annotationName) != null
+            if(component._annotations_.scannedTemplate != null){
+                if (component._annotations_.scannedTemplate.originalType == ComponentOriginalType.Component) {
+                    return component._annotations_.clazz.getAnnotation(annotationName) != null
+                }
             }
         }
         return false
